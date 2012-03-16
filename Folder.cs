ede051d26bfed5bb93e216d07d7f02431e9585d2
Geoffrey Vancoetsem <geoff@geoffzone.net>
2012-03-16 18:03:11 +0100

    Parcours du applicationHost terminé. Principe de suppression partiel. Principe de compression à développer.

---
+++ Folder.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Web.Administration;
+using System.IO;
+using System.Diagnostics;
+
+namespace Smartgeek.LogRotator
+{
+	public class Folder
+	{
+		public enum IisServiceType
+		{
+			W3SVC,
+			FTPSVC,
+			SMTPSVC
+		}
+
+		public enum PeriodType
+		{
+			MaxSize = 0,
+			Daily = 1,
+			Weekly = 2,
+			Monthly = 3,
+			Hourly = 4
+		}
+
+		private Folder ()
+		{
+
+		}
+
+		public long? SiteID { get; private set; }
+		public bool Enabled { get; private set; }
+		public bool IsCustomFormat { get; private set; }
+		public String Directory { get; private set; }
+		public String FileLogFormat { get; private set; }
+		public PeriodType Period { get; private set; }
+		public bool IsLocalTimeRollover { get; private set; }
+		public long TruncateSize { get; private set; }
+
+		public static Folder Create(ConfigurationElement logFileElement, IisServiceType type, bool isUTF8, bool isCentralW3C = false, bool isCentralBinary = false, long? siteId = null)
+		{
+			String dir = (String)logFileElement["directory"];
+			String subdir = type.ToString("G");
+			PeriodType periodType = (PeriodType)logFileElement["period"];
+			String fileprefix = isCentralBinary ? null : isUTF8 ? "u_" : null;
+			String filesuffix = ".log";
+			bool isCustomFormat = false;
+
+			if (isCentralW3C)
+			{
+				fileprefix += (periodType == PeriodType.MaxSize) ? "extend" : "ex";
+			}
+			else if (isCentralBinary)
+			{
+				fileprefix = (periodType == PeriodType.MaxSize) ? "raw" : "ra";
+				filesuffix = ".ibl";
+			}
+			else
+			{
+				subdir += siteId.Value.ToString();
+				fileprefix += (periodType == PeriodType.MaxSize) ? "extend" : "ex";
+
+				if (type != IisServiceType.FTPSVC)
+				{
+					switch ((int)logFileElement["logFormat"])
+					{
+						case 0: fileprefix += (periodType == PeriodType.MaxSize) ? "inetsv" : "in"; break;
+						case 1: fileprefix += (periodType == PeriodType.MaxSize) ? "ncsa" : "nc"; break;
+						case 3: isCustomFormat = true; break;
+					}
+				}
+			}
+
+			String fileformat = "{0}";
+
+			switch (periodType)
+			{
+				case PeriodType.Hourly: fileformat = "{0:yyMMddhh}"; break;
+				case PeriodType.Daily: fileformat = "{0:yyMMdd}"; break;
+				case PeriodType.Weekly: fileformat = "{0:yyMMww}"; break;
+				case PeriodType.Monthly: fileformat = "{0:yyMM}"; break;
+			}
+
+			String directory = null;
+			String fileLogFormat = null;
+			if (!isCustomFormat)
+			{
+				directory = Path.Combine(
+					dir,
+					subdir
+				);
+				fileLogFormat = String.Concat(
+					fileprefix,
+					fileformat,
+					filesuffix
+				);
+			}
+
+			return new Folder()
+			{
+				SiteID = siteId,
+				Enabled = (bool)logFileElement["enabled"],
+				IsCustomFormat = isCustomFormat,
+				Directory = directory,
+				FileLogFormat = fileLogFormat,
+				Period = periodType,
+				IsLocalTimeRollover = (bool)logFileElement["localTimeRollover"],
+				TruncateSize = (long)logFileElement["truncateSize"]
+			};
+		}
+	}
+}
--- Program.cs
+++ Program.cs
@@ -8,20 +8,23 @@ using System.Globalization;
 using Microsoft.Web.Administration;
 using Smartgeek.LogRotator.Configuration;
 using System.Diagnostics;
+using System.IO;
 
 namespace Smartgeek.LogRotator
 {
 	class Program
 	{
-		static void Main(string[] args)
+		private const int MaxMissingCount = 100;
+
+		private static List<Folder> s_folders = new List<Folder>();
+
+		private static void Main(string[] args)
 		{
 			Thread.CurrentThread.Priority = ThreadPriority.BelowNormal;
 			Thread.CurrentThread.CurrentCulture = CultureInfo.InvariantCulture;
 
-			List<ConfigurationElement> logFileElements = new List<ConfigurationElement>();
-
-			using (ServerManager serverManager = new ServerManager())
-			//using (ServerManager serverManager = ServerManager.OpenRemote("rovms502"))
+			//using (ServerManager serverManager = new ServerManager())
+			using (ServerManager serverManager = ServerManager.OpenRemote("rovms502"))
 			{
 				// applicationHost.config
 				Microsoft.Web.Administration.Configuration config = serverManager.GetApplicationHostConfiguration();
@@ -30,14 +33,15 @@ namespace Smartgeek.LogRotator
 				ConfigurationSection logSection = config.GetSection("system.applicationHost/log");
 
 				// central log file ?
-				bool isCentralLog;
+				bool isCentralW3C = false;
+				bool isCentralBinary = false;
 				ConfigurationElement centralLogFile = null;
 				ConfigurationAttribute centralLogFileModeAttr = logSection.GetAttribute("centralLogFileMode");
 				switch ((int)centralLogFileModeAttr.Value)
 				{
-					case 0: isCentralLog = false; break;
-					case 1: isCentralLog = true; centralLogFile = logSection.GetChildElement("centralBinaryLogFile"); break;
-					case 2: isCentralLog = true; centralLogFile = logSection.GetChildElement("centralW3CLogFile"); break;
+					case 0: break;
+					case 1: isCentralBinary = true; centralLogFile = logSection.GetChildElement("centralBinaryLogFile"); break;
+					case 2: isCentralW3C = true; centralLogFile = logSection.GetChildElement("centralW3CLogFile"); break;
 					default: throw new NotSupportedException("The value " + centralLogFileModeAttr.Value + " for system.applicationHost/log/@centralLogFileMode is not supported");
 				}
 
@@ -49,13 +53,13 @@ namespace Smartgeek.LogRotator
 				ConfigurationSection ftpServerLogSection = config.GetSection("system.ftpServer/log");
 
 				// FTP central log file ?
-				bool isFtpServerCentralLog;
+				bool isFtpServerCentralW3C = false;
 				ConfigurationElement ftpServerCentralLogFile = null;
 				ConfigurationAttribute ftpServerCentralLogFileModeAttr = ftpServerLogSection.GetAttribute("centralLogFileMode");
 				switch ((int)ftpServerCentralLogFileModeAttr.Value)
 				{
-					case 0: isFtpServerCentralLog = false; break;
-					case 1: isFtpServerCentralLog = true; ftpServerCentralLogFile = ftpServerLogSection.GetChildElement("centralLogFile"); break;
+					case 0: break;
+					case 1: isFtpServerCentralW3C = true; ftpServerCentralLogFile = ftpServerLogSection.GetChildElement("centralLogFile"); break;
 					default: throw new NotSupportedException("The value " + ftpServerCentralLogFileModeAttr.Value + " for system.ftpServer/log/@centralLogFileMode is not supported");
 				}
 
@@ -63,21 +67,28 @@ namespace Smartgeek.LogRotator
 				bool isFtpServerUTF8 = (bool)ftpServerLogSection.GetAttributeValue("logInUTF8"); 
 				#endregion
 
-				if (isCentralLog)
+				if (isCentralW3C || isCentralBinary)
 				{
-					// TODO central log file processing
-					//throw new NotImplementedException();
-					logFileElements.Add(centralLogFile);
+					s_folders.Add(Folder.Create(
+						centralLogFile,
+						Folder.IisServiceType.W3SVC,
+						isUTF8,
+						isCentralW3C: isCentralW3C,
+						isCentralBinary: isCentralBinary
+					));
 				}
 
-				if (isFtpServerCentralLog)
+				if (isFtpServerCentralW3C)
 				{
-					// TODO FTP central log file processing
-					//throw new NotImplementedException();
-					logFileElements.Add(ftpServerCentralLogFile);
+					s_folders.Add(Folder.Create(
+						ftpServerCentralLogFile,
+						Folder.IisServiceType.FTPSVC,
+						isFtpServerUTF8,
+						isCentralW3C: isFtpServerCentralW3C
+					));
 				}
 
-				if (!isCentralLog || !isFtpServerCentralLog)
+				if (!isCentralW3C || !isCentralBinary || !isFtpServerCentralW3C)
 				{
 					// per-site log file processing
 					ConfigurationSection sitesSection = config.GetSection("system.applicationHost/sites");
@@ -85,6 +96,7 @@ namespace Smartgeek.LogRotator
 
 					foreach (ConfigurationElement site in sitesCollection)
 					{
+						long siteId = (long)site["id"];
 						ConfigurationElementCollection bindingsCollection = site.GetCollection("bindings");
 						
 						// any http/https binding ?
@@ -94,71 +106,145 @@ namespace Smartgeek.LogRotator
 							return StringComparer.InvariantCultureIgnoreCase.Equals(protocol, "http") || StringComparer.InvariantCultureIgnoreCase.Equals(protocol, "https");
 						});
 
-						if (isWebSite && !isCentralLog)
+						if (isWebSite && !(isCentralW3C || isCentralBinary))
 						{
 							ConfigurationElement logFile = site.GetChildElement("logFile");
-							// TODO site log file processing
-							logFileElements.Add(logFile);
+							s_folders.Add(Folder.Create(
+								logFile,
+								Folder.IisServiceType.W3SVC,
+								isUTF8,
+								siteId: siteId
+							));
 						}
 
-						// any ftp/ftps binding ?
+						// any ftp binding ?
 						bool isFtpSite = bindingsCollection.Any(binding =>
 						{
 							String protocol = (String)binding.GetAttributeValue("protocol");
-							return StringComparer.InvariantCultureIgnoreCase.Equals(protocol, "ftp") || StringComparer.InvariantCultureIgnoreCase.Equals(protocol, "ftps");
+							return StringComparer.InvariantCultureIgnoreCase.Equals(protocol, "ftp");
 						});
 
-						if (isFtpSite && !isFtpServerCentralLog)
+						if (isFtpSite && !isFtpServerCentralW3C)
 						{
 							ConfigurationElement ftpServer = site.GetChildElement("ftpServer");
 							ConfigurationElement ftpServerLogFile = ftpServer.GetChildElement("logFile");
-							// TODO FTP site log file processing
-							logFileElements.Add(ftpServerLogFile);
+							s_folders.Add(Folder.Create(
+								ftpServerLogFile,
+								Folder.IisServiceType.FTPSVC,
+								isFtpServerUTF8,
+								siteId: siteId
+							));
 						}
 					}
 				}
-
-				/*
-						 * 
-						 * ConfigurationAttribute enabledAttr = logFile.GetAttribute("enabled");
-						if (!String.IsNullOrEmpty(logFile.GetAttributeValue("customLogPluginClsid") as String))
-							continue;
-						
-
-						// TODO handle other periods
-						if (log.Period != LoggingRolloverPeriod.Daily)
-							continue;
-
-						RotationSettingsElement rotationSettings = RuntimeConfig.Rotation.GetSiteSettingsOrDefault(site.Id);
-
-						bool isFtp = site.Bindings.Any(b => StringComparer.InvariantCultureIgnoreCase.Equals(b.Protocol, "ftp") || StringComparer.InvariantCultureIgnoreCase.Equals(b.Protocol, "ftps"));
-						if (isFtp)
-						{
-						
-						}
-
-						bool isHttp = site.Bindings.Any(b => StringComparer.InvariantCultureIgnoreCase.Equals(b.Protocol, "http") || StringComparer.InvariantCultureIgnoreCase.Equals(b.Protocol, "https"));
-						if (isHttp)
-						{
-
-						}*/
 			}
 
-			for (int index = 0, count = logFileElements.Count; index < count; index++)
-			{
-				ConfigurationElement logFileElement = logFileElements[index];
+			s_folders.ForEach(ProcessFolder);
+		}
 
-				Debug.WriteLine(
-					"{0}. customLogPluginClsid={1}, directory={2}, enabled={3}, localTimeRollover={4}, logFormat={5}, period={6}, truncateSize={7}",
-					(index + 1),
-					logFileElement.GetAttributeValue2("customLogPluginClsid"),
-					logFileElement.GetAttributeValue2("directory"),
-					logFileElement.GetAttributeValue2("enabled"),
-					logFileElement.GetAttributeValue2("localTimeRollover"),
-					logFileElement.GetAttributeValue2("logFormat"),
-					logFileElement.GetAttributeValue2("period"),
-					logFileElement.GetAttributeValue2("truncateSize")
-				);
+		private static void ProcessFolder(Folder folder)
+		{
+			Debug.WriteLine(
+				"Folder: Enabled={0}, IsCustomFormat={1}, Directory={2}, FileLogFormat={3}, Period={4}, IsLocalTimeRollover={5}, TruncateSize={6}",
+				folder.Enabled,
+				folder.IsCustomFormat,
+				folder.Directory,
+				folder.FileLogFormat,
+				folder.Period,
+				folder.IsLocalTimeRollover,
+				folder.TruncateSize
+			);
+
+			if (!folder.Enabled)
+			{
+				Console.Out.WriteLine("Skipping folder {0} because logging is disabled", folder.Directory);
+				return;
+			}
+
+			if (folder.IsCustomFormat)
+			{
+				Console.Out.WriteLine("Skipping folder {0} because custom logging is used", folder.Directory);
+				return;
+			}
+			
+			// specific site rotation settings or default settings
+			RotationSettingsElement settings;
+			if (folder.SiteID.HasValue)
+			{
+				settings = RuntimeConfig.Rotation.GetSiteSettingsOrDefault(folder.SiteID.Value);
+			}
+			else
+			{
+				settings = RuntimeConfig.Rotation.DefaultSettings;
+			}
+
+			// datetime references
+			bool useUTC = !folder.IsLocalTimeRollover;
+			DateTime now = useUTC ? DateTime.Now.ToUniversalTime() : DateTime.Now;
+			DateTime compressAfterDate = settings.Compress ? now.AddDays(settings.CompressAfter) : DateTime.MaxValue;
+			DateTime deleteAfterDate = settings.Delete ? now.AddDays(settings.DeleteAfter) : DateTime.MaxValue;
+
+			// file size rollover
+			if (folder.Period == Folder.PeriodType.MaxSize)
+			{
+				for (int index = 1; ; index++)
+				{
+					String filename = Path.Combine(folder.Directory, String.Format(folder.FileLogFormat, index));
+					String nextFilename = Path.Combine(folder.Directory, String.Format(folder.FileLogFormat, index + 1));
+
+					// break on last log file
+					// we don't know if IIS is still using it
+					if (!File.Exists(nextFilename))
+					{
+						Console.Out.Write("Skipping last file {0}", filename);
+						Trace.TraceInformation("{0} skipped (last file)", filename);
+						break;
+					}
+
+					FileInfo fi = new FileInfo(filename);
+
+					// deletion
+					if ((useUTC && fi.CreationTimeUtc > deleteAfterDate) || (!useUTC && fi.CreationTime > deleteAfterDate))
+					{
+						Console.Out.Write("Deleting {0}... ", filename);
+						try
+						{
+							//fi.Delete();
+							Trace.TraceInformation("{0} deleted", filename);
+							Console.Out.WriteLine("OK");
+						}
+						catch (Exception ex)
+						{
+							Trace.TraceError("{0} delete error: {1}", filename, ex.Message);
+							Trace.TraceError(ex.ToString());
+							Console.Out.WriteLine("ERROR: {0}", ex.Message);
+						}
+					}
+
+					// compression
+					/*if (((useUTC && fi.CreationTimeUtc > compressAfterDate) || (!useUTC && fi.CreationTime > compressAfterDate)) && fi.Attributes )
+					{
+						Console.Out.Write("Compressing {0}... ", filename);
+						try
+						{
+							//fi.Delete();
+							Trace.TraceInformation("{0} compressed", filename);
+							Trace.TraceInformation("{0} deleted", filename);
+							Console.Out.WriteLine("OK");
+						}
+						catch (Exception ex)
+						{
+							Trace.TraceError("{0} delete error: {1}", filename, ex.Message);
+							Trace.TraceError(ex.ToString());
+							Console.Out.WriteLine("ERROR: {0}", ex.Message);
+						}
+					}*/
+				}
+			}
+			// time-based rollover
+			else
+			{
+				// TODO
 			}
 		}
 	}
